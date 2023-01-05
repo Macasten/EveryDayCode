82b5b74fd1cd2a66def24beb61e2a4a4f2ccaf55
Filipe Goncalves <filipe.goncalves@gmail.com>
2023-01-05 14:03:21 +0000

    [PLAYGROUND] Environment setup

---
+++ playground/playground.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <utility>
+
+int main(){
+
+    std::cout << "!!Playground!!" << std::endl;
+    // Playground Start
+
+    // Playground Ended
+    return 0;
+}
