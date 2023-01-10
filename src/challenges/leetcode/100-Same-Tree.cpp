31870ac516723133896a776999535f428a9ddca5
Filipe Goncalves <filipe.goncalves@gmail.com>
2023-01-10 07:49:43 +0000

    [PROJECT] Added include files on leetcode daily challenges

--- src/challenges/leetcode/100-Same-Tree.cpp
+++ src/challenges/leetcode/100-Same-Tree.cpp
@@ -4,17 +4,8 @@
 #include <vector>
 #include <utility>
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#include "TreeNode.h"
+
 class Solution {
 public:
 
@@ -30,3 +21,7 @@ public:
         }
     }
 };
+
+int main(){
+  return 0;
+}
