b94f24b3908cdd555f595131d1b71ecb39651746
Filipe Goncalves <filipe.goncalves@gmail.com>
2023-02-16 07:26:49 +0000

    [LEETCODE]: Add create tree for tree node

--- src/challenges/leetcode/includes/TreeNode.h
+++ src/challenges/leetcode/includes/TreeNode.h
@@ -1,13 +1,50 @@
 #ifndef TREENODE_H
 #define TREENODE_H
 
+#include <vector>
+#include <string>
+
 // Definition for a binary tree node.
-struct TreeNode {
+struct TreeNode
+{
   int val;
   TreeNode *left;
   TreeNode *right;
   TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+
+  static void *buildTreeNode(TreeNode *root, const std::vector<std::string> &arr, int index)
+  {
+    if (root != nullptr)
+    {
+      if (2 * index + 1 < arr.size())
+      {
+        if (arr.at(2 * index + 1) == "null")
+          root->left = nullptr;
+        else
+          root->left = new TreeNode(std::stoi(arr.at(2 * index + 1)));
+        buildTreeNode(root->left, arr, 2 * index + 1);
+      }
+
+      if (2 * index + 2 < arr.size())
+      {
+        if (arr.at(2 * index + 2) == "null")
+          root->right = nullptr;
+        else
+          root->right = new TreeNode(std::stoi(arr.at(2 * index + 2)));
+        buildTreeNode(root->right, arr, 2 * index + 2);
+      }
+    }
+  }
+
+  static TreeNode *buildTreeNode(std::vector<std::string> arr)
+  {
+    if (arr.size() == 0)
+      return nullptr;
+    TreeNode *output = new TreeNode(std::stoi(arr.at(0)));
+    buildTreeNode(output, arr, 0);
+    return output;
+  }
 };
 #endif /* TREENODE_H */
